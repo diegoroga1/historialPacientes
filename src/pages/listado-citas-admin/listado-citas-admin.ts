be1cdd79b7c5f2e6c30628b4a45bbc910b17b053
Jose Vicente <jvicentecab@hotmail.com>
2017-03-26 23:55:56 +0100

    Añadida lista de pacientes pendientes para admin 2

---
+++ src/pages/listado-citas-admin/listado-citas-admin.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-listado-citas-admin',
+  templateUrl: 'listado-citas-admin.html'
+})
+export class ListadoCitasAdminPage {
+
+    citas = [
+                {id: 1,
+                paciente: 'Pepe Pérez',
+                especialidadDemandada: 'Médico de cabecera', 
+                medico: 'Antonio Pérez', 
+                fecha: '12/02/17', 
+                prioridad: 'Alta'},
+                {id: 2, 
+                paciente: 'Juan Rodríguez',
+                especialidadDemandada: 'Traumatología', 
+                medico: 'Antonio Gutiérrez', 
+                fecha: '24/11/16', 
+                prioridad: 'Baja'},
+                {id: 3, 
+                paciente: 'María Sosa',
+                especialidadDemandada: 'Psicología', 
+                medico: 'José García', 
+                fecha: '17/06/16', 
+                prioridad: 'Media'},
+                {id: 4, 
+                paciente: 'Lola Ginés',
+                especialidadDemandada: 'Urgencias', 
+                medico: 'María Dolores', 
+                fecha: '10/05/15', 
+                prioridad: 'Alta'},
+                {id: 5, 
+                paciente: 'Pérez Reverte',
+                especialidadDemandada: 'Nefrología', 
+                medico: 'José Medina', 
+                fecha: '12/03/15', 
+                prioridad: 'Baja'},
+                {id: 6, 
+                paciente: 'Andrés Montes',
+                especialidadDemandada: 'Alergología', 
+                medico: 'Laura Silva', 
+                fecha: '07/04/14', 
+                prioridad: 'Alta'}
+  ];
+  
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  
+  accederCita() {
+    alert("Has accedido a la cita");
+  }
+
+}
