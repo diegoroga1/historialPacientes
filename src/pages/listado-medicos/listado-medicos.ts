4f50c8481296442ae65c786c68f1ed5254d9d34a
IgnacioGCL <159357258123456Is>
2017-04-27 19:53:37 +0100

    Filtrado de especialidades

--- src/pages/listado-medicos/listado-medicos.ts
+++ src/pages/listado-medicos/listado-medicos.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
 import {AngularFire, FirebaseListObservable} from "angularfire2";
+import {Subject} from "rxjs";
 
 /*
-  Generated class for the ListadoMedicos page.
+ Generated class for the ListadoMedicos page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+ See http://ionicframework.com/docs/v2/components/#navigation for more info on
+ Ionic pages and navigation.
+ */
 @Component({
   selector: 'page-listado-medicos',
   templateUrl: 'listado-medicos.html'
@@ -15,9 +16,18 @@ import {AngularFire, FirebaseListObservable} from "angularfire2";
 export class ListadoMedicosPage {
 
   todos: FirebaseListObservable<any>;
+  subject = new Subject();
+
   constructor(public navCtrl: NavController, public navParams: NavParams, private firebase: AngularFire) {
-    this.todos = firebase.database.list('/usuarios');
+    this.todos = firebase.database.list('/usuarios', {
+      query: {
+        orderByChild: 'especialidad',
+        equalTo: this.subject
+      }
+    });
   }
 
-
+  selectMed(especialidad) {
+    this.subject.next(especialidad);
+  }
 }
